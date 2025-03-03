f082714ab829809ee5e9a5e9aa0890e0e7d04256
Ahmad Zufar <a.zufarazmi@gmail.com>
2025-03-03 13:57:55 +0800

    update browser router

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 import "@fontsource/outfit";
 import "@fontsource/roboto";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
